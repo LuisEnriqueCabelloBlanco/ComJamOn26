671b66f4373d545a0809422bf7b1f177e0d68dae
Diego Montenegro <dmonoficial22@email.com>
2026-02-21 15:59:09 +0100

    ajustes señales

--- adn/Scripts/MutationSceneController.gd
+++ adn/Scripts/MutationSceneController.gd
@@ -5,10 +5,6 @@ signal patern1(b: bool)
 signal patern2(b: bool)
 signal patern3(b: bool)
 
-
-func _ready() -> void:
-	GameManager.pattern1.connect(test1d)
-
 ## Lanza la señal 1 al game manager
 func signalToGM1(b: bool):
 	GameManager.pattern1.emit(b)
@@ -20,16 +16,3 @@ func signalToGM2(b: bool):
 ## Lanza la señal 3 al game manager
 func signalToGM3(b: bool):
 	GameManager.pattern3.emit(b)
-
-# Para ver si las señales funcionan
-func test1d(b: bool):
-	if b:
-		print(" patron 1")
-
-func test2d(b: bool):
-	if b:
-		print(" patron 2")
-
-func test3d(b: bool):
-	if b:
-		print(" patron 3")
