22431442bf7a0e4e956c125b5d6bdddb02ae33e0
LuisEnriqueCabelloBlanco <luisecab@ucm.es>
2026-02-22 12:51:45 +0100

    Animaciones!!!!

--- adn/Scenes/Components/animaton_component.gd
+++ adn/Scenes/Components/animaton_component.gd
@@ -3,20 +3,40 @@ extends Node
 
 @export_subgroup("Nodes")
 @export var sprite:AnimatedSprite2D
+@export var frontHorn:AnimatedSprite2D
+@export var backHorn:AnimatedSprite2D
+@export var eye:AnimatedSprite2D
 
 func handle_horizontal_flip(move_dir: float)->void:
 	if move_dir==0:
 		return
 	sprite.flip_h = false if move_dir > 0 else true
+	frontHorn.flip_h = false if move_dir > 0 else true
+	backHorn.flip_h = false if move_dir > 0 else true
+	eye.flip_h = false if move_dir > 0 else true
 	
 func handle_move_animaton(move_direcction:float)->void:
 	handle_horizontal_flip(move_direcction)
 	if move_direcction != 0:
 		sprite.play("default")
+		frontHorn.play("default")
+		backHorn.play("default")
+		eye.play("default")
+	else:
+		sprite.play("idle")
+		frontHorn.play("idle")
+		backHorn.play("idle")
+		eye.play("idle")
 
 func handle_jump_animation(is_jumping:bool, is_falling:bool)->void:
 	if is_jumping:
 		sprite.play("jumpUp")
+		frontHorn.play("jumpUp")
+		backHorn.play("jumpUp")
+		eye.play("jumpUp")
 	elif is_falling:
 		sprite.play("jumpDown")
+		frontHorn.play("jumpDown")
+		backHorn.play("jumpDown")
+		eye.play("jumpDown")
 	pass
